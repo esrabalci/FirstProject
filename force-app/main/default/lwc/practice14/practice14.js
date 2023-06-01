2175ebb8f3b52cfbe57e986cc5e3c0429f58e347
esrabalci <esrambalci@hotmail.com>
2023-06-01 10:27:28 -0400

    homework - filteredAccount - slider -spinning

---
+++ force-app/main/default/lwc/practice14/practice14.js
@@ -0,0 +1,50 @@
+import { LightningElement } from "lwc";
+import { ShowToastEvent } from "lightning/platformShowToastEvent";
+export default class Practice14 extends LightningElement {
+  showSpinner = false;
+
+  handleClick() {
+    // set the value of showSpinner to true
+    this.showSpinner = true;
+    // wait 3 seconds and make it false
+    // eslint-disable-next-line @lwc/lwc/no-async-operation
+    setTimeout(() => {
+      this.showSpinner = false;
+      this.handleShowSuccessToast();
+    }, 3000);
+  }
+
+/**
+ * In order ro show toast message in LWC, need to import
+  import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+     create a ShowToastEvent object
+   provide constructor parameter with all the details of toast message
+     createMethod(){
+       title -> toast title
+       message -> the detail of the toast
+       variant -> success, warning, info, error
+     }
+ */
+
+  handleShowSuccessToast() {
+    // Create show toast event object and provide details
+    const event = new ShowToastEvent({
+      title: "Awesome Success Title",
+      message: "Your awesome actions has succeed! HIRED!!",
+      variant: "success"
+    });
+    // eventually dispatch this event manually so it can show the toast message
+    this.dispatchEvent(event);
+  }
+
+  handleShowErrorToast() {
+    // Create show toast event object and provide details
+    const event = new ShowToastEvent({
+      title: "Awesome Error Title",
+      message: "Your awesome actions has failed! TRY AGAIN!!",
+      variant: "error"
+    });
+    // eventually dispatch this event manually so it can show the toast message
+    this.dispatchEvent(event);
+  }
+}
---
+++ force-app/main/default/lwc/practice15/practice15.js
@@ -0,0 +1,12 @@
+import { LightningElement } from "lwc";
+export default class Practice15 extends LightningElement {
+
+
+
+
+
+
+    
+
+
+}
