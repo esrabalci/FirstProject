062c2ebfd430d3a30ad460a2eba77d2095522e78
esrabalci <esrambalci@hotmail.com>
2023-06-06 10:48:50 -0400

    delete record-refreshApex

--- force-app/main/default/lwc/practice28/practice28.js
+++ force-app/main/default/lwc/practice28/practice28.js
@@ -2,29 +2,33 @@ import getAllAccounts from '@salesforce/apex/AccountController.getAllAccounts';
 import { deleteRecord } from 'lightning/uiRecordApi';
 import { LightningElement, wire } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+//in order to force refresh wired data result
+//we need to import refresh method from @salesforce/apex
+import { refreshApex } from "@salesforce/apex"
 
 export default class Practice28 extends LightningElement {
 
-    allAccounts; 
-    error; 
+    // allAccounts; 
+    // error; 
 
     @wire(getAllAccounts)
-    wiredAccount({ data, error }) {
+    allAccounts;
         
-        if (data) {
-            this.allAccounts = data; 
-            this.error = undefined; 
-        } else if (error) {
-            this.error = error; 
-            this.allAccounts = undefined; 
-        }
+    // wiredAccount(data,error)  {
+        //  if (data) {
+    //         this.allAccounts = data; 
+    //         this.error = undefined; 
+    //     } else if (error) {
+    //         this.error = error; 
+    //         this.allAccounts = undefined; 
+    //     }
 
-    } 
+    // } 
 
     handleDelete(event) {
         // in order to access attribute data-xxxx
         // in this case data.id 
-        // we use event.detail.dataset
+        // we use event.target.dataset.whatevernameThatYouGive
         console.log('Button clicked');
         console.log(event.target.dataset.id);
         const recordId = event.target.dataset.id; 
@@ -40,6 +44,13 @@ export default class Practice28 extends LightningElement {
                 message: 'Successfully deleted',
                 variant: 'success'
             }));
+        // the list is not refreshed after deletion 
+        // TODO : somehow refresh the wired result
+        // so the wire method get called again by force
+        // since it' cached it will not be called again 
+        // unless instructed to do so
+
+            refreshApex(this.allAccounts);
 
         }  )
     }
